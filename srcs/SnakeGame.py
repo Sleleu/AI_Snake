d55b65c7dce931652f33d4fc1594b2d8f62359cb
Sleleu <93100775+Sleleu@users.noreply.github.com>
2025-01-27 13:56:41 +0100

    norm flake8 on SnakeGame.py

--- srcs/SnakeGame.py
+++ srcs/SnakeGame.py
@@ -1,7 +1,7 @@
 import pygame as pg
 import numpy as np
 from .Colors import Colors as Col
-from settings import *
+from settings import CELL_SIZE, GRID_SIZE, MARGIN, WIDTH, HEIGHT, FPS
 
 
 class SnakeGame:
@@ -28,7 +28,7 @@ class SnakeGame:
         b += f"model: {self.model}\n"
         b += f"training: {self.training}\n"
         return b
-    
+
     def init_game(self):
         pg.init()
         pg.display.set_caption('Learn2Slither')
@@ -66,7 +66,8 @@ class SnakeGame:
             self.clock.tick(FPS)
 
     def move_snake(self):
-        new_head = [h + a for h, a in zip(self.snake_head, self.actions[self.direction])]
+        new_head = [h + a for h, a in zip(self.snake_head,
+                                          self.actions[self.direction])]
         self.snake_body.insert(0, new_head)
         self.snake_head = self.snake_body[0]
         self.snake_body.pop()
@@ -82,7 +83,6 @@ class SnakeGame:
             case pg.K_RIGHT:
                 self.direction = "RIGHT"
 
-
     def place_items(self):
         self.grid.fill(0)
         for i, snake_part in enumerate(self.snake_body):
@@ -90,6 +90,7 @@ class SnakeGame:
 
     def draw_game(self):
         self.surface.fill(Col.BG_COLOR)
+
         def draw_grid():
             for row in range(self.grid.shape[0]):
                 for column in range(self.grid.shape[1]):
@@ -98,19 +99,29 @@ class SnakeGame:
                                         CELL_SIZE,
                                         CELL_SIZE)
                     if (row + column) % 2 == 0:
-                        pg.draw.rect(self.surface, Col.GRID_COLOR_EVEN, grid_cell)
+                        pg.draw.rect(surface=self.surface,
+                                     color=Col.GRID_COLOR_EVEN,
+                                     rect=grid_cell)
                     else:
-                        pg.draw.rect(self.surface, Col.GRID_COLOR_ODD, grid_cell)
+                        pg.draw.rect(surface=self.surface,
+                                     color=Col.GRID_COLOR_ODD,
+                                     rect=grid_cell)
+
         def draw_snake():
             for snake_part in self.snake_body:
                 snake_cell = pg.Rect(snake_part[1] * CELL_SIZE + MARGIN,
                                      snake_part[0] * CELL_SIZE + MARGIN,
                                      CELL_SIZE,
                                      CELL_SIZE)
+
                 if snake_part == self.snake_head:
-                    pg.draw.rect(self.surface, Col.SNAKE_HEAD_COLOR, snake_cell)
+                    pg.draw.rect(surface=self.surface,
+                                 color=Col.SNAKE_HEAD_COLOR,
+                                 rect=snake_cell)
                 else:
-                    pg.draw.rect(self.surface, Col.SNAKE_COLOR, snake_cell)
+                    pg.draw.rect(surface=self.surface,
+                                 color=Col.SNAKE_COLOR,
+                                 rect=snake_cell)
 
         draw_grid()
         draw_snake()
